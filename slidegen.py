42db7cd11022c5ff40946a3061c0ff4f54fbbeb5
larryhou <larryhou@foxmail.com>
2018-11-16 10:05:54 +0800

    Add slide number option

--- slidegen.py
+++ slidegen.py
@@ -22,6 +22,20 @@ class SlideTheme(enum.Enum):
             if isinstance(value, SlideTheme): choices.append(name)
         return choices
 
+def camel(s:str)->str:
+    result = ''
+    flag = False
+    for c in s:
+        if c in ('_', '-'):
+            flag = True
+            continue
+        if flag:
+            result += c.upper()
+            flag = False
+        else:
+            result += c
+    return result
+
 if __name__ == '__main__':
     import argparse, sys
     arguments = argparse.ArgumentParser()
@@ -40,7 +54,8 @@ if __name__ == '__main__':
     arguments.add_argument('--var-controls-layout')
     arguments.add_argument('--var-controls-back-arrows')
     arguments.add_argument('--var-progress', choices=['true', 'false'])
-    arguments.add_argument('--var-slide-number', choices=['true', 'false'])
+    arguments.add_argument('--var-slide-number', default='c/t', choices=['true','false','c/t', 'c', 'h/v', 'h.v'])
+    arguments.add_argument('--var-show-slide-number', default='all', choices=['all', 'speaker', 'print'])
     arguments.add_argument('--var-history', choices=['true', 'false'])
     arguments.add_argument('--var-keyboard', choices=['true', 'false'])
     arguments.add_argument('--var-overview', choices=['true', 'false'])
@@ -93,9 +108,11 @@ if __name__ == '__main__':
     for name, value in vars(options).items():
         if name.startswith('var_') and value:
             name = re.sub(r'^var_', '', name)
-            assert name in default_options, name
-            item = default_options.get(name)
-            config[item[0]] = value
+            if name in default_options:
+                name = default_options.get(name)[0]
+            else:
+                name = camel(name)
+            config[name] = value
 
     command = 'pandoc -s --mathjax -i -t revealjs'
     for name, value in config.items():
@@ -105,6 +122,21 @@ if __name__ == '__main__':
     command += ' -o {}/index.html'.format(slide_name)
     print('+ {}'.format(command))
     assert os.system(command) == 0
+    slide_number_style = options.var_slide_number
+    slide_number_pattern = re.compile(r'(slideNumber:\s*)({})(\s*,)'.format(slide_number_style))
+    import tempfile
+    temp = open(tempfile.mktemp('_slide.html'), 'w+')
+    with open('{}/index.html'.format(slide_name), 'r') as fp:
+        for line in fp.readlines():
+            if slide_number_style not in ('true', 'false') and slide_number_pattern.search(line):
+                line = slide_number_pattern.sub(r'\g<1>{!r}\g<3>'.format(slide_number_style), line)
+            temp.write(line)
+    temp.seek(0)
+    print(fp.name)
+    with open(fp.name, 'w') as fp:
+        fp.write(temp.read())
+    os.remove(temp.name)
+
 
 
 
